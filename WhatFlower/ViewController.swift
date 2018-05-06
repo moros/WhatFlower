f0d0b1487600c4c37c64936b2f20fc1ae1b0a61c
Doug <androidsoong@gmail.com>
2018-05-06 11:11:45 -0500

    Xcode project creation.

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Doug Mason on 5/6/18.
+//  Copyright © 2018 Doug Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
