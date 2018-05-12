3184386d850937b3cfb8dfe8b05c1ed3dfb7074f
Doug <androidsoong@gmail.com>
2018-05-12 18:14:45 -0500

    Added UIImagePicker, to select photo from either camera, photo library or saved photo album.

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,10 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            print("There was an error picking the image.")
+            return
+        }
+        
+        imageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraBarButtonPressed(_ sender: UIBarButtonItem)
+    {
+        let alertController = UIAlertController(title: "What should the source be for the image picker?",
+                                                message: "The image picker has different source options, camera, photo library or saved photos album.",
+                                                preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Saved Photos Album", style: .default, handler: { (action) in
+            self.imagePicker.sourceType = .savedPhotosAlbum
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
